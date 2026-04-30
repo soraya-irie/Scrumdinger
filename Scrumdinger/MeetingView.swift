27be378453118a687a5c7505f0fbc401d1c9e356
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 18:58:49 +0900

    Complete Section2 Step8 - using stacks to arrange views

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -5,8 +5,12 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                Text("Seconds Elapsed")
-                Text("Seconds Remaining")
+                VStack {
+                    Text("Seconds Elapsed")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                }
             }
         }
     }
