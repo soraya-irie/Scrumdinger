7039e1031690858159edbcef6b45b640335872e7
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 19:15:45 +0900

    Complete Section3 Step5 - using stacks to arrange views

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -19,6 +19,9 @@ struct MeetingView: View {
             }
             Circle()
                 .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+            }
         }
     }
 }
