4839f73149fccafdf0582d7094bc663b0b2955ba
soraya-ire <rujiangkongshi@gmail.com>
2026-04-30 18:29:47 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by soraya irie on 2026/04/30.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
