c1bc19d4b61a5fd6d67d8c3cd2513f6b86c1b137
soraya-ire <rujiangkongshi@gmail.com>
2026-05-11 16:09:28 +0900

    Complete Section6 Step3 - creating a card view

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -14,6 +14,7 @@ struct CardView: View {
                     .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
                     .labelStyle(.trailingIcon)
             }
             .font(.caption)
