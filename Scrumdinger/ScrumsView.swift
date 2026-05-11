6841bd3f2fee26aa7f4df0ea6e88793b42a18c3c
soraya-ire <rujiangkongshi@gmail.com>
2026-05-11 16:26:24 +0900

    Complete Section1 Step4 - displaying data in a list

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -4,7 +4,9 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(scrums, id: \.title) { scrum in
+
+        }
     }
 }
 
