f097fe4b41c40c620fbcab2f2b332b0b8493fccc
soraya-ire <rujiangkongshi@gmail.com>
2026-05-11 16:21:08 +0900

    Complete Section1 Step2 - displaying data in a list

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct ScrumsView: View {
+    let scrums: [DailyScrum]
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
