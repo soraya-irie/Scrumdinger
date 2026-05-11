5673d1609aaab615dee807d3330d551f040440bd
soraya-ire <rujiangkongshi@gmail.com>
2026-05-11 16:55:48 +0900

    Complete Section1 Step1 - creating-a-navigation-hierarchy

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -5,9 +5,11 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
 
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
-                .listRowBackground(scrum.theme.mainColor)
+        NavigationStack {
+            List(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.theme.mainColor)
+            }
         }
     }
 }
