ac9e1f0a3dc4696930a40de36742bd8aa16a6f13
soraya-ire <rujiangkongshi@gmail.com>
2026-05-11 15:16:46 +0900

    Complete Section3 Step7 - creating a card view

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -4,7 +4,8 @@ import ThemeKit
 struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(scrum.title)
+            .font(.headline)
     }
 }
 
