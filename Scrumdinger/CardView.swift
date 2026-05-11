e6a75af417dbf9b6cd52dc0fc2941f5a1afbfd9c
soraya-ire <rujiangkongshi@gmail.com>
2026-05-11 15:31:05 +0900

    Complete Section3 Step11 - creating a card view

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -10,7 +10,7 @@ struct CardView: View {
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
-                Label("Label", systemImage: "clock")
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
             }
         }
     }
