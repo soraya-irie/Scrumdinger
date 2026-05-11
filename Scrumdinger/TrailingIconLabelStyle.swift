f1a711b36c3aae3bac4d16457f5f8f14ac00cd07
soraya-ire <rujiangkongshi@gmail.com>
2026-05-11 15:51:49 +0900

    Complete Section5 Step6 - creating a card view

--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct TrailingIconLabelStyle: LabelStyle {
     func makeBody(configuration: Configuration) -> some View {
         HStack {
-
+            configuration.title
+            configuration.icon
         }
     }
 }
