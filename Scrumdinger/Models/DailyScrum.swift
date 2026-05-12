177ed0f0f0aef9bb97e5a6778868b6893d1807af
soraya-ire <rujiangkongshi@gmail.com>
2026-05-12 13:06:45 +0900

    Complete Section4 Step3 - creating-a-navigation-hierarchy

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -21,5 +21,10 @@ extension DailyScrum {
     struct Attendee: Identifiable {
         let id: UUID
         var name: String
+
+        init(id: UUID = UUID(), name: String) {
+            self.id = id
+            self.name = name
+        }
     }
 }
