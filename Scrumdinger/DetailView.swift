dbd7bb6ad75588bb29751fa6089921cc4d20b1a9
soraya-ire <rujiangkongshi@gmail.com>
2026-05-15 18:15:27 +0900

    Complete Section5 Step1 - creating-a-navigation-hierarchy

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -7,9 +7,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
