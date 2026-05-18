c2ad396e402ae1b83c2c86b183980b0308b0ea2f
soraya-ire <rujiangkongshi@gmail.com>
2026-05-18 13:12:09 +0900

    Complete Section4 Step1 - creating the edit view

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -4,6 +4,8 @@ import ThemeKit
 struct DetailView: View {
     let scrum: DailyScrum
 
+    @State private var isPresentingEditView = false
+
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
