043a08353fcbc58ca9380f83ada788818f1d799e
soraya-ire <rujiangkongshi@gmail.com>
2026-05-18 14:09:14 +0900

    Complete Section1 Step8 - passing data with bindings

--- Scrumdinger/Views/ThemeView.swift
+++ Scrumdinger/Views/ThemeView.swift
@@ -6,6 +6,7 @@ struct ThemeView: View {
 
     var body: some View {
         Text(theme.name)
+            .padding(4)
             .frame(maxWidth: .infinity)
             .background(theme.mainColor)
             .foregroundColor(theme.accentColor)
