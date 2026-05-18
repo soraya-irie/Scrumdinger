b6b34b54349b386ef03cd54da8923518cacc69f2
soraya-ire <rujiangkongshi@gmail.com>
2026-05-18 12:25:06 +0900

    Complete Section1 Step4 - creating the edit view

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -4,7 +4,10 @@ struct DetailEditView: View {
     @State private var scrum = DailyScrum.emptyScrum
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Meeting Info")) {
+            }
+        }
     }
 }
 
