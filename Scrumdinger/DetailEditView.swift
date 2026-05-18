a7e9631431b43ac4d54b87ae0453f84c153e2bfe
soraya-ire <rujiangkongshi@gmail.com>
2026-05-18 12:19:38 +0900

    Complete Section1 Step3 - creating the edit view

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
