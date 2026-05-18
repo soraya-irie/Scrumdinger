a1bddfb7a39cfaa3ea0e3c6fd9729b5290a7670c
soraya-ire <rujiangkongshi@gmail.com>
2026-05-18 14:14:24 +0900

    Complete Section2 Step2 - passing data with bindings

--- Scrumdinger/Views/ThemePicker.swift
+++ Scrumdinger/Views/ThemePicker.swift
@@ -1,6 +1,9 @@
 import SwiftUI
+import ThemeKit
 
 struct ThemePicker: View {
+    @Binding var selection: Theme
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
