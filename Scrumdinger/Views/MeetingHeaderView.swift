abca98e90c71a48c456a9333e1bf7af49c8f350d
soraya-ire <rujiangkongshi@gmail.com>
2026-05-19 14:09:24 +0900

    Complete Section2 Step3 - managing state and life cycle

--- Scrumdinger/Views/MeetingHeaderView.swift
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -2,10 +2,26 @@ import SwiftUI
 
 struct MeetingHeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ProgressView(value: 5, total: 15)
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Seconds Elapsed")
+                    .font(.caption)
+                Label("300", systemImage: "hourglass.tophalf.fill")
+            }
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("Seconds Remaining")
+                    .font(.caption)
+                Label("600", systemImage: "hourglass.bottomhalf.fill")
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time remaining")
+        .accessibilityValue("10 minutes")
     }
 }
 
-#Preview {
+#Preview(traits: .sizeThatFitsLayout) {
     MeetingHeaderView()
 }
