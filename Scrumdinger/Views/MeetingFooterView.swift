d8daa5952985e197853b3300a697728e88990a32
soraya-ire <rujiangkongshi@gmail.com>
2026-05-20 12:11:15 +0900

    Complete Section6 Step1 - managing state and life cycle

---
+++ Scrumdinger/Views/MeetingFooterView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        HStack {
+            Text("Speaker 1 of 3")
+            Spacer()
+            Button(action: {}) {
+                Image(systemName: "forward.fill")
+            }
+            .accessibilityLabel("Next speaker")
+        }
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    MeetingFooterView()
+}
