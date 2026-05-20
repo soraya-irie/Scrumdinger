eafd34b4db82d12f5e31eef561545b8321e0ed57
soraya-ire <rujiangkongshi@gmail.com>
2026-05-20 12:58:27 +0900

    Complete Section6 Step7 - managing state and life cycle

--- Scrumdinger/Views/MeetingFooterView.swift
+++ Scrumdinger/Views/MeetingFooterView.swift
@@ -17,13 +17,19 @@ struct MeetingFooterView: View {
         return "Speaker \(speakerNumber) of \(speakers.count)"
     }
     var body: some View {
-        HStack {
-            Text(speakerText)
-            Spacer()
-            Button(action: {}) {
-                Image(systemName: "forward.fill")
+        VStack {
+            HStack {
+                if isLastSpeaker {
+                    Text("Last Speaker")
+                } else {
+                    Text(speakerText)
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
+                }
             }
-            .accessibilityLabel("Next speaker")
         }
     }
 }
