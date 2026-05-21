0ab3bfe7825fa8f46c1ee8b3c22eb0ab5589b2a3
soraya-ire <rujiangkongshi@gmail.com>
2026-05-21 12:58:22 +0900

    Complete Section7 Step5 - managing state and life cycle

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -28,6 +28,7 @@ struct MeetingView: View {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendeeNames: scrum.attendees.map { $0.name })
             scrumTimer.speakerChangedAction = {
                 player.seek(to: .zero)
+                player.play()
             }
             scrumTimer.startScrum()
         }
