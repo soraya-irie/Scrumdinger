176b6cbe8384a5b263c2cf7728033d2e7a47b6b7
soraya-ire <rujiangkongshi@gmail.com>
2026-05-22 12:21:01 +0900

    Complete Section1 Step9 - updating app data

--- Scrumdinger/Views/NewScrumSheet.swift
+++ Scrumdinger/Views/NewScrumSheet.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct NewScrumSheet: View {
+    @State private var newScrum = DailyScrum.emptyScrum
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
