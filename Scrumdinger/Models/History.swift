e9f2926cebc4ffdda2b4b742a0cf91fd31293c91
soraya-ire <rujiangkongshi@gmail.com>
2026-05-22 15:19:22 +0900

    Complete Section1 Step6 - persisting data

--- Scrumdinger/Models/History.swift
+++ Scrumdinger/Models/History.swift
@@ -6,6 +6,7 @@ class History: Identifiable {
     var id: UUID
     var date: Date
     var attendees: [Attendee]
+    var dailyScrum: DailyScrum?
 
     init(id: UUID = UUID(), date: Date = Date(), attendees: [Attendee]) {
         self.id = id
