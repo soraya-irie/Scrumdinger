4f8fa312b92a8bc253f71aa29880f889b444f801
soraya-ire <rujiangkongshi@gmail.com>
2026-05-22 14:24:24 +0900

    Complete Section2 Step7 - updating app data

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -37,6 +37,9 @@ struct DetailView: View {
                     Label(attendee.name, systemImage: "person")
                 }
             }
+            Section(header: Text("History")) {
+
+            }
         }
         .navigationTitle(scrum.title)
         .toolbar {
