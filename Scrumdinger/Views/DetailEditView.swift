d3ff2d7e642a326350bf3c4963125d946c5c787b
soraya-ire <rujiangkongshi@gmail.com>
2026-05-22 15:21:16 +0900

    Complete Section1 Step7 - persisting data

--- Scrumdinger/Views/DetailEditView.swift
+++ Scrumdinger/Views/DetailEditView.swift
@@ -33,7 +33,7 @@ struct DetailEditView: View {
                     TextField("New Attendee", text: $attendeeName)
                     Button(action: {
                         withAnimation {
-                            let attendee = DailyScrum.Attendee(name: attendeeName)
+                            let attendee = Attendee(name: attendeeName)
                             scrum.attendees.append(attendee)
                             attendeeName = ""
                         }
