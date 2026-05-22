990c9488f41ef350494e9e3aa81dace89f134ccb
soraya-ire <rujiangkongshi@gmail.com>
2026-05-22 13:52:09 +0900

    Complete Section2 Step2 - updating app data

--- Scrumdinger/Models/History.swift
+++ Scrumdinger/Models/History.swift
@@ -4,4 +4,10 @@ struct History: Identifiable {
     let id: UUID
     let date: Date
     var attendees: [DailyScrum.Attendee]
+
+    init(id: UUID = UUID(), date: Date = Date(), attendees: [DailyScrum.Attendee]) {
+        self.id = id
+        self.date = date
+        self.attendees = attendees
+    }
 }
