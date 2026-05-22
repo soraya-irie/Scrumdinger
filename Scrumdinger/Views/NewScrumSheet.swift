2e502226c80ee5471dbcfd771adb4767fcf06daf
soraya-ire <rujiangkongshi@gmail.com>
2026-05-22 17:04:42 +0900

    Complete Section3 Step9 - persisting data

--- Scrumdinger/Views/NewScrumSheet.swift
+++ Scrumdinger/Views/NewScrumSheet.swift
@@ -1,18 +1,14 @@
 import SwiftUI
 
 struct NewScrumSheet: View {
-    @State private var newScrum = DailyScrum.emptyScrum
-    @Binding var scrums: [DailyScrum]
 
     var body: some View {
         NavigationStack {
-            DetailEditView(scrum: $newScrum, saveEdits: { dailyScrum in
-                scrums.append(newScrum)
-            })
+            DetailEditView(scrum: nil)
         }
     }
 }
 
 #Preview {
-    NewScrumSheet(scrums: .constant(DailyScrum.sampleData))
+    NewScrumSheet()
 }
