469a645efe3f73cf72e08a2b99c360f1f908f420
soraya-ire <rujiangkongshi@gmail.com>
2026-05-23 15:30:01 +0900

    Complete Section4 Step6 - handling errors

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -8,7 +8,6 @@ struct ScrumdingerApp: App {
         WindowGroup {
             ScrumsView()
         }
-//        .modelContainer(for: DailyScrum.self)
-        .modelContainer(try! .init(for: DailyScrum.self, configurations: .init(allowsSave: false)))
+        .modelContainer(for: DailyScrum.self)
     }
 }
