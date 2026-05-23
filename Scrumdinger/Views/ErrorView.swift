004179b22408eed5b9fb7676df21d5b1fb23489b
soraya-ire <rujiangkongshi@gmail.com>
2026-05-23 13:58:47 +0900

    Complete Section2 Step2 - handling errors

--- Scrumdinger/Views/ErrorView.swift
+++ Scrumdinger/Views/ErrorView.swift
@@ -1,6 +1,8 @@
 import SwiftUI
 
 struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
