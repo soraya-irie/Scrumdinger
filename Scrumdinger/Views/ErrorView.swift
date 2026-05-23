6b95e129a240b58d24446e0d7939e66c5364e9b9
soraya-ire <rujiangkongshi@gmail.com>
2026-05-23 14:11:09 +0900

    Complete Section2 Step6 - handling errors

--- Scrumdinger/Views/ErrorView.swift
+++ Scrumdinger/Views/ErrorView.swift
@@ -5,7 +5,9 @@ struct ErrorView: View {
 
     var body: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Text("An error has occurred!")
+                .font(.title)
+                .padding(.bottom)
             Spacer()
         }
         .padding()
