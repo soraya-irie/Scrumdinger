7c487ce1fd62f6a102264cd2853454a55d673b6e
soraya-ire <rujiangkongshi@gmail.com>
2026-05-23 16:26:33 +0900

    Complete Section2 Step4 - drawing the timer view

--- Scrumdinger/Views/SpeakerArk.swift
+++ Scrumdinger/Views/SpeakerArk.swift
@@ -4,6 +4,10 @@ struct SpeakerArk: Shape {
     let speakerIndex: Int
     let totalSpeakers: Int
 
+    private var degreesPerSpeaker: Double {
+        360.0 / Double(totalSpeakers)
+    }
+
     func path(in rect: CGRect) -> Path {
         return Path { path in
         }
