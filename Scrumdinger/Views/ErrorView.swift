659a64ceca1457335adfa74775058055c80e256e
soraya-ire <rujiangkongshi@gmail.com>
2026-05-23 14:07:30 +0900

    Complete Section2 Step5 - handling errors

--- Scrumdinger/Views/ErrorView.swift
+++ Scrumdinger/Views/ErrorView.swift
@@ -4,7 +4,11 @@ struct ErrorView: View {
     let errorWrapper: ErrorWrapper
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Spacer()
+        }
+        .padding()
     }
 }
 
