6e08edd1f266260ff5b5ded5a361fefc5e02add4
soraya-ire <rujiangkongshi@gmail.com>
2026-05-23 15:48:39 +0900

    Complete Section1 Step4 - drawing the timer view

--- Scrumdinger/Views/MeetingTimerView.swift
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -1,6 +1,11 @@
 import SwiftUI
+import ThemeKit
+import TimerKit
 
 struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    let theme: Theme
+
     var body: some View {
         Circle()
             .strokeBorder(lineWidth: 24)
@@ -11,5 +16,6 @@ struct MeetingTimerView: View {
 }
 
 #Preview {
-    MeetingTimerView()
+    let speakers = [ScrumTimer.Speaker(name: "Bill", isCompleted: true), ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
+    MeetingTimerView(speakers: speakers, theme: .yellow)
 }
