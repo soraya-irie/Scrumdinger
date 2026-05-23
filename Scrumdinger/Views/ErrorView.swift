b20c09af50120530c6ba875e7630097379bb6a8b
soraya-ire <rujiangkongshi@gmail.com>
2026-05-23 14:15:56 +0900

    Complete Section2 Step8 - handling errors

--- Scrumdinger/Views/ErrorView.swift
+++ Scrumdinger/Views/ErrorView.swift
@@ -10,6 +10,9 @@ struct ErrorView: View {
                 .padding(.bottom)
             Text(errorWrapper.error.localizedDescription)
                 .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
             Spacer()
         }
         .padding()
