fec14694888111d20e48d8274cd254ac16a82213
soraya-ire <rujiangkongshi@gmail.com>
2026-05-23 16:41:48 +0900

    Complete Section2 Step9 - drawing the timer view

--- Scrumdinger/Views/SpeakerArk.swift
+++ Scrumdinger/Views/SpeakerArk.swift
@@ -17,6 +17,7 @@ struct SpeakerArk: Shape {
     func path(in rect: CGRect) -> Path {
         let diameter = min(rect.size.width, rect.size.height) - 24.0
         let radius = diameter / 2.0
+        let center = CGPoint(x: rect.midX, y: rect.midY)
         return Path { path in
         }
     }
