4a1b8e4871b2e3bac7c7684593a10e449f2fda09
soraya-ire <rujiangkongshi@gmail.com>
2026-05-24 11:57:47 +0900

    Complete Section4 Step4 - transcribing speech to text

--- Scrumdinger/Views/HistoryView.swift
+++ Scrumdinger/Views/HistoryView.swift
@@ -9,5 +9,11 @@ struct HistoryView: View {
 }
 
 #Preview {
-    HistoryView()
+    let history = History(attendees: [
+        Attendee(name: "Jon"),
+        Attendee(name: "Darla"),
+        Attendee(name: "Luis")
+    ],
+                          transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI...")
+    HistoryView(history: history)
 }
