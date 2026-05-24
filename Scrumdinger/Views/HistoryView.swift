fec1ca3601727c58a6b259146afffff981493ffb
soraya-ire <rujiangkongshi@gmail.com>
2026-05-24 12:13:47 +0900

    Complete Section4 Step9 - transcribing speech to text

--- Scrumdinger/Views/HistoryView.swift
+++ Scrumdinger/Views/HistoryView.swift
@@ -11,6 +11,10 @@ struct HistoryView: View {
                 Text("Attendees")
                     .font(.headline)
                 Text(history.attendeeString)
+                if let transcript = history.transcript {
+                    Text("Transcript")
+                    Text(transcript)
+                }
             }
         }
     }
