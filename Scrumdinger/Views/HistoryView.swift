5597d630d32c40604913d64fe14ca40e2a0b417f
soraya-ire <rujiangkongshi@gmail.com>
2026-05-24 12:00:03 +0900

    Complete Section4 Step6 - transcribing speech to text

--- Scrumdinger/Views/HistoryView.swift
+++ Scrumdinger/Views/HistoryView.swift
@@ -6,6 +6,8 @@ struct HistoryView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
             }
         }
     }
