a3c0fbf9bb1a5134da491ee16be13d834d9a7a30
soraya-ire <rujiangkongshi@gmail.com>
2026-05-24 11:47:20 +0900

    Complete Section3 Step3 - transcribing speech to text

--- Scrumdinger/Views/MeetingTimerView.swift
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -19,6 +19,7 @@ struct MeetingTimerView: View {
                     Text(currentSpeaker)
                         .font(.title)
                     Text("is speaking")
+                    Image(systemName: isRecording ? "mic" : "mic.slash")
                 }
                 .accessibilityElement(children: .combine)
                 .foregroundStyle(theme.accentColor)
